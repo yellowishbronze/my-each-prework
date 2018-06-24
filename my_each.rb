9b3c44164974c0758db775eb12f8bb7b47240850
Bryan Teng <bteng1226@gmail.com>
2018-06-24 21:25:18 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  index =0
+  while index < array.length
+    yield([array[index])
+    index+=1
+  end
+
 end
+
+my_each([1,2,3])
